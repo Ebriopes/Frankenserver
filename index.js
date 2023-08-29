9c65bc9286ff2f67372aee1d09b967bfa6ad5e73
ebriopes <antonio.vargasrosales@gmail.com>
2023-08-29 00:49:20 -0600

    🍱 Chore: initialize the server

--- index.js
+++ index.js
@@ -1,15 +1,3 @@
-require( 'dotenv' ).config();
-const express = require( 'express' );
-const serveIndex = require( 'serve-index' );
+const { app, PORT } = require('./server/server');
 
-const port = process.env.PORT || 3000;
-const app = express();
-
-app.use( '/public', express.static( 'public' ) );
-app.use( '/public', serveIndex( 'public' ) );
-
-app.get( '/', ( _, res ) => {
-  res.send( "Hi, you are in my server! What you need?" );
-} );
-
-app.listen( port, () => console.log( `Server listening in port ${ port }` ) );
+app.listen(PORT, () => console.log(`Server listening in port ${PORT}`));
