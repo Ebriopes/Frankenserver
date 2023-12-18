48f135366fd78d770103358a2b7f6456e2bbde1b
Daniel Vargas <Antonio.vargasrosales@gmail.com>
2023-12-17 23:34:26 -0600

    ✏️Chore: Update API response to GET "/login"

--- src/routes/login.ts
+++ src/routes/login.ts
@@ -6,7 +6,7 @@ const router = Router();
 router.get("/", (_, res) => {
   res
     .status(200)
-    .send("Oh you do want enter, eh. Try with a POST method instead.");
+    .send("Oh do you want enter, eh? Try with a POST method instead.");
 });
 
 router.post("/", AuthController.login);
